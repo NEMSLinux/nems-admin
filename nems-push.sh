c5d1b7ac6574e1c9470ff437d26b1676d3930d01
Cat5TV <contact@category5.tv>
2017-10-03 09:47:11 -0400

    Update

--- nems-push.sh
+++ nems-push.sh
@@ -1,21 +1,17 @@
 #!/bin/bash
 
 cd /root/nems/nems-admin
-git add *
-git commit -m "Update"
+git commit -am "Update"
 git push origin master
 
 cd /home/pi/nems-scripts/
-git add *
-git commit -m "Update"
+git commit -am "Update"
 git push origin master
 
 cd /var/www/html/
-git add *
-git commit -m "Update"
+git commit -am "Update"
 git push origin master
 
 cd /root/nems/nems-migrator/
-git add *
-git commit -m "Update"
+git commit -am "Update"
 git push origin master
