73f34ffbfb1f6f5c244a4c2b443ee1ed3c429aab
Robbie Ferguson <contact@category5.tv>
2017-11-17 08:25:48 -0500

    Update nems-prep.sh

--- nems-prep.sh
+++ nems-prep.sh
@@ -2,6 +2,9 @@
 # Prepare a deployment for NEMS Installation
 # This is the firstrun script which simply installs the needed repositories
 
+# Run like this:
+# wget -O /tmp/nems-prep.sh https://raw.githubusercontent.com/Cat5TV/nems-admin/master/nems-prep.sh && chmod +x /tmp/nems-prep.sh && /tmp/nems-prep.sh
+
 # run as root
 
 cd /root
