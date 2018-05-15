3dd41b4dc9bef06c126a057c826e46a28d7d5860
NEMS Linux <nems@baldnerd.com>
2018-05-15 20:26:15 +0000

    Organize

--- nems-build.sh
+++ nems-build.sh
@@ -56,69 +56,27 @@ do
   apt --yes --no-install-recommends install $pkg
 done
 
+# Install dependencies, if any
 apt --yes install -f
 
 # Be up to date
 apt --yes upgrade && apt --yes dist-upgrade
 
-# Create nemsadmin user
-adduser --disabled-password --gecos "" nemsadmin
-# Allow user to become super-user
-usermod -aG sudo nemsadmin
-# Set the user password
-echo -e "nemsadmin\nnemsadmin" | passwd nemsadmin >/tmp/init 2>&1
-
-# Delete any non-root user (eg: pi)
-userdel -f -r pi
-userdel -f -r test #armbian
-userdel -f -r odroid
-userdel -f -r rock64
-userdel -f -r linaro # ASUS TinkerBoard
-userdel -f -r dietpi
-
 # Disable firstrun
 systemctl disable firstrun
 rm /etc/init.d/firstrun # ARMbian
 
+# Setup Linux user
+  ./build/30-user
+
 # Install Nagios Core
-  useradd nagios
-  groupadd nagcmd
-  usermod -a -G nagios www-data
-  usermod -a -G nagcmd nagios
-  usermod -a -G nagcmd www-data
-  cd /tmp
-  wget https://assets.nagios.com/downloads/nagioscore/releases/nagios-4.3.4.tar.gz
-  tar -zxvf nagios-4.3.4.tar.gz
-  cd /tmp/nagios-4.3.4/
-  ./configure --with-nagios-group=nagios --with-command-group=nagcmd --with-httpd_conf=/etc/apache2/conf-available/
-  make all
-  make install
-  make install-init
-  make install-config
-  make install-commandmode
-  make install-webconf
+  ./build/50-nagios
 
-  cd /tmp
-  wget https://nagios-plugins.org/download/nagios-plugins-2.2.1.tar.gz
-  tar -zxvf /tmp/nagios-plugins-2.2.1.tar.gz
-  cd /tmp/nagios-plugins-2.2.1/
-  ./configure --with-nagios-user=nagios --with-nagios-group=nagios
-  make
-  make install
-
-  a2enmod rewrite
-  a2enmod cgi
-  a2enconf nagios
-
-  systemctl start nagios
-  systemctl enable nagios
-
-# Finished installing Nagios Core
+# Install Check_MK livestatus
+  ./build/55-check_mk-livestatus
 
 # Install cockpit
   ./build/110-cockpit
-
-exit
 
 # Setup NEMS software
   ./build/150-nems
@@ -148,11 +106,6 @@ cd /tmp
   make all
   make install
 
-# Migrate NEMS' customizations such as Nagios theme and icons
-
-# Import package configurations from NEMS-Migrator
-
-# Import default data from NEMS-Migrator
 
 # Enable systemd items
 systemctl enable webmin
@@ -160,20 +113,12 @@ systemctl enable webmin
 # clean it up!
 apt --yes autoremove
 
-# Add nemsadmin to sudoers and disable root login if that's successful
-usermod -aG sudo nemsadmin && passwd -l root
-
-# Add files to nemsadmin home folder (which later get moved to NEMS user account at init)
-cd /home/nemsadmin
-wget -O license.txt https://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
-cp /root/nems/nems-migrator/data/nems/changelog.txt .
-
 echo "Usage after build:"
 df -hT /etc
 
-echo "Don't forget to run: echo DEVID > /etc/.nems_hw_model_identifier"
-
 # Output any errors encountered along the way.
 cat /tmp/errors.log
+
+echo "Don't forget to run: echo DEVID > /etc/.nems_hw_model_identifier"
 
 fi
