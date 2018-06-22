6e187f758365cbd09da1370edfa497e73cffd68d
NEMS Linux <nems@baldnerd.com>
2018-06-22 15:42:48 +0000

    Move locale

--- nems-prep.sh
+++ nems-prep.sh
@@ -27,17 +27,6 @@ else
 
   apt update
 
-  # Generate locale
-  apt install -y locales
-  export LANGUAGE=en_US.UTF-8
-  export LANG=en_US.UTF-8
-  export LC_ALL=en_US.UTF-8
-  if grep -q "# en_US.UTF-8" /etc/locale.gen; then
-    /bin/sed -i -- 's,# en_US.UTF-8,en_US.UTF-8,g' /etc/locale.gen
-  fi
-  locale-gen
-  #dpkg-reconfigure locales
-
   apt install --yes git screen dialog gnupg nano apt-utils
   
   # Setup default account info
