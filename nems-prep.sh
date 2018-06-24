87898d0cc8ae4a8b4cb95c19f444a570425375db
Robbie Ferguson <contact@category5.tv>
2018-06-24 18:22:18 -0400

    Add missing environment variable for locale

--- nems-prep.sh
+++ nems-prep.sh
@@ -46,12 +46,13 @@ else
   export LANGUAGE=en_US.UTF-8
   export LANG=en_US.UTF-8
   export LC_ALL=en_US.UTF-8
+  export LC_TIME=en_US.UTF-8
   if grep -q "# en_US.UTF-8" /etc/locale.gen; then
     /bin/sed -i -- 's,# en_US.UTF-8,en_US.UTF-8,g' /etc/locale.gen
   fi
   locale-gen
-  #dpkg-reconfigure locales
-
+  #dpkg-reconfigure locales # Set second screen to UTF8
+  
   # Make it so SSH does not load the locale from the connecting machine (causes problems on Pine64)
   # This requires the user to re-connect
   sed -i -e 's/    SendEnv LANG LC_*/#   SendEnv LANG LC_*/g' /etc/ssh/ssh_config
