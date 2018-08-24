a1ad3d94d99e4d7df8ee299567ebcef572dd4b54
NEMS Linux <nems@baldnerd.com>
2018-08-24 13:43:55 -0400

    test

--- nems-push.sh
+++ nems-push.sh
@@ -2,10 +2,15 @@
 DATE=`date +%Y-%m-%d`
 if [[ "$1" == '' ]]; then comment="Update $DATE"; else comment=$1; fi
 
+echo Pushing NEMS update: $comment
+echo ""
+read -p "Github Password: " password
+
 cd /root/nems/nems-admin
 git add *
 git commit -am "$comment"
-git push origin master
+#git push origin master
+git push 'https://Cat5TV:$password@github.com/Cat5TV/nems-admin.git'
 
 cd /usr/local/share/nems/nems-scripts/
 git add *
