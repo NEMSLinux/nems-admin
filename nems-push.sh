c73f501913a1eefc8f7e078cbe2d49a6bb98047c
NEMS Linux <nems@baldnerd.com>
2018-08-24 13:46:20 -0400

    nems-push now prompts for password only once

--- nems-push.sh
+++ nems-push.sh
@@ -15,26 +15,31 @@ git push "https://Cat5TV:$password@github.com/Cat5TV/nems-admin.git"
 cd /usr/local/share/nems/nems-scripts/
 git add *
 git commit -am "$comment"
-git push origin master
+#git push origin master
+git push "https://Cat5TV:$password@github.com/Cat5TV/nems-scripts.git"
 
 cd /var/www/html/
 git add *
 git commit -am "$comment"
-git push origin master
+#git push origin master
+git push "https://Cat5TV:$password@github.com/Cat5TV/nems-www.git"
 
 cd /root/nems/nems-migrator/
 git add *
 git commit -am "$comment"
-git push origin master
+#git push origin master
+git push "https://Cat5TV:$password@github.com/Cat5TV/nems-migrator.git"
 
 cd /var/www/nconf/
 git add *
 git commit -am "$comment"
-git push origin develop
+#git push origin develop
+git push "https://Cat5TV:$password@github.com/Cat5TV/nconf.git"
 
 if [[ -d /root/nems/nems-tools ]]; then
   cd /root/nems/nems-tools
   git add *
   git commit -am "$comment"
-  git push origin master
+  #git push origin master
+  git push "https://Cat5TV:$password@github.com/Cat5TV/nems-tools.git"
 fi
