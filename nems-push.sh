fc73981d07b6008420f574348c9dbc012a285f24
NEMS Linux <nems@baldnerd.com>
2018-08-26 23:55:32 +0000

    support entire lineup of Pine64

--- nems-push.sh
+++ nems-push.sh
@@ -7,30 +7,40 @@ echo ""
 read -s -p "Github Password: " password
 
 cd /root/nems/nems-admin
+echo ""
+pwd
 git add *
 git commit -am "$comment"
 #git push origin master
 git push "https://Cat5TV:$password@github.com/Cat5TV/nems-admin.git"
 
 cd /usr/local/share/nems/nems-scripts/
+echo ""
+pwd
 git add *
 git commit -am "$comment"
 #git push origin master
 git push "https://Cat5TV:$password@github.com/Cat5TV/nems-scripts.git"
 
 cd /var/www/html/
+echo ""
+pwd
 git add *
 git commit -am "$comment"
 #git push origin master
 git push "https://Cat5TV:$password@github.com/Cat5TV/nems-www.git"
 
 cd /root/nems/nems-migrator/
+echo ""
+pwd
 git add *
 git commit -am "$comment"
 #git push origin master
 git push "https://Cat5TV:$password@github.com/Cat5TV/nems-migrator.git"
 
 cd /var/www/nconf/
+echo ""
+pwd
 git add *
 git commit -am "$comment"
 #git push origin develop
@@ -38,6 +48,8 @@ git push "https://Cat5TV:$password@github.com/Cat5TV/nconf.git"
 
 if [[ -d /root/nems/nems-tools ]]; then
   cd /root/nems/nems-tools
+  echo ""
+  pwd
   git add *
   git commit -am "$comment"
   #git push origin master
