a99dd09cb9ff0ead96347f3d434c63288fdc5059
NEMS Linux <nems@baldnerd.com>
2019-03-16 10:16:32 -0400

    Create PID before delay timer

--- nems-cleanup.sh
+++ nems-cleanup.sh
@@ -264,11 +264,8 @@ nameserver 2001:4860:4860::8844
   # This script removes any MOTD that were added during installs (annoying!)
   /root/nems/nems-admin/build/230-motd
 
-  # Clear patches.log
-  if [[ -e /var/log/nems/patches.log ]]; then
-    rm /var/log/nems/patches.log
-  fi
-  touch /var/log/nems/patches.log
+  # Remove resize from patches.log
+  /bin/sed -i~ '/PATCH-000002/d' /var/log/nems/patches.log
 
   # Cleanup lingering logs and so-on
   /root/nems/nems-admin/build/999-cleanup
