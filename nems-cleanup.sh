3962fc64a4b5ccc081978a53feacd0beade1643a
NEMS Linux <nems@baldnerd.com>
2019-03-19 12:47:54 -0400

    Reset Network Config

--- nems-cleanup.sh
+++ nems-cleanup.sh
@@ -266,6 +266,9 @@ nameserver 2001:4860:4860::8844
   # This script removes any MOTD that were added during installs (annoying!)
   /root/nems/nems-admin/build/230-motd
 
+  # Clear all network interface configs
+  /usr/local/share/nems/nems-scripts/reset-network-manager.sh
+
   # Cleanup lingering logs and so-on
   /root/nems/nems-admin/build/999-cleanup
 
