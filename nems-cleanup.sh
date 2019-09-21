0d23ee3d1a23d3541668fe7d633a3116931f138a
Robbie Ferguson <contact@category5.tv>
2019-09-21 14:57:59 -0400

    Automatically remove old kernels during cleanup

--- nems-cleanup.sh
+++ nems-cleanup.sh
@@ -97,10 +97,12 @@ else
   sudo apt-get autoclean
   apt-get autoremove
 
-  echo "Don't forget to remove the old kernels:"
-  dpkg --get-selections | grep linux-image
+  #echo "Don't forget to remove the old kernels:"
+  #dpkg --get-selections | grep linux-image
+  echo "Removing old kernels..."
+  apt-get remove --purge -y $(dpkg -l 'linux-*' | sed '/^ii/!d;/'"$(uname -r | sed "s/\(.*\)-\([^0-9]\+\)/\1/")"'/d;s/^[^ ]* [^ ]* \([^ ]*\).*/\1/;/[0-9]/!d')
 
-  # Delete all Samba users
+# Delete all Samba users
   pdbedit -L | while read USER; do pdbedit -x -u $(echo $USER | cut -d: -f1); done
 
   # Empty old logs
