341889a759c532c9071d73a75be9d3d3dd2f72b8
NEMS Linux <nems@baldnerd.com>
2020-05-30 22:06:28 -0400

    Move sample conf to 1.6 branch

--- nems-push.sh
+++ nems-push.sh
@@ -7,7 +7,7 @@ echo ""
 read -s -p "Github Password: " password
 
 ram=$(grep MemTotal /proc/meminfo | awk '{print $2 * 1024 - 100000000}')
-ramMB=$(grep MemTotal /proc/meminfo | awk '{print $2 / 1024 - 100000000}')
+ramMB=$(grep MemTotal /proc/meminfo | awk '{print $2 - 100000000}')
 echo ""
 echo ""
 echo "Setting postBuffer to $ram bytes ($ramMB MB)."
