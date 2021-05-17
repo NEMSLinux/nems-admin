312688fe5957b468388243234ceec85896da75af
NEMS Linux <nems@baldnerd.com>
2021-05-17 16:36:06 -0400

    Fix looking for MySQL DB

--- nems-restore-sample-db.sh
+++ nems-restore-sample-db.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
-version=$(/usr/local/bin/nems-info nemsbranch)
-#version=1.4
 db=Sample
 #db=Clean
 
@@ -8,7 +6,7 @@ db=Sample
 
 systemctl stop mysql
 rm -rf /var/lib/mysql
-cp -R /root/nems/nems-migrator/data/$version/mysql/NEMS-$db /var/lib/
+cp -R /root/nems/nems-migrator/data/mysql/NEMS-$db /var/lib/
 chown -R mysql:mysql /var/lib/NEMS-$db
 mv /var/lib/NEMS-$db /var/lib/mysql
 systemctl start mysql
@@ -16,7 +14,7 @@ systemctl start mysql
 systemctl stop nagios
 rm -rf /etc/nems/conf/Default_collector
 rm -rf /etc/nems/conf/global
-cp -R /root/nems/nems-migrator/data/$version/nagios/conf/* /etc/nems/conf/
+cp -R /root/nems/nems-migrator/data/nagios/conf/* /etc/nems/conf/
 
 # Set ownership
 chown -R www-data:www-data /etc/nems/conf/Default_collector
